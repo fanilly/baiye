ab7abc8a4037fbbd7ab27fcce1f38bea9a30277f
fanilly <815969707@qq.com>
2018-08-13 16:28:46 +0800

    修改获取坐标模块

--- src/api/getCoordinate.js
+++ src/api/getCoordinate.js
@@ -13,25 +13,28 @@ export default function() {
         jsApiList: ['getLocation', 'openlocation']
       });
     });
-    console.log('sssssss')
-    setTimeout(()=>{
-      console.log('success --')
-      resolve({
-        latitude: 34.79977,
-        longitude: 113.66072
-      })
-    },2000);
-    // wx.ready(function() {
-    //   console.log('ready')
-    //   wx.getLocation({
-    //     type: 'wgs84',
-    //     success: function(res) {
-    //       console.log('get location success')
-    //       let latitude = res.latitude;
-    //       let longitude = res.longitude;
-    //       resolve({ latitude, longitude });
-    //     }
-    //   });
-    // });
+    const isDev = process.env.NODE_ENV === 'development';
+    if (isDev) {
+      setTimeout(() => {
+        resolve({
+          latitude: 34.79977,
+          longitude: 113.66072
+        })
+      }, 2000);
+    } else {
+      wx.ready(function() {
+        console.log('ready')
+        wx.getLocation({
+          type: 'wgs84',
+          success: function(res) {
+            console.log('get location success')
+            let latitude = res.latitude;
+            let longitude = res.longitude;
+            resolve({ latitude, longitude });
+          }
+        });
+      });
+    }
+
   });
 }
